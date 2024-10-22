9ea89b6184b02dc2d98f8cf0b6826667fb5557a4
Oscar Soto <oscaralejandrosoto9@gmail.com>
2024-10-22 14:07:36 -0500

    Interfaz modificada

---
+++ src/java/Modelo/Empleado.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author oscar
+ */
+public class Empleado {
+    private int idEmpleado;
+    private String nombre;
+    private String apellido;
+    private double salario;
+
+    public Empleado(int idEmpleado, String nombre, String apellido, double salario) {
+        this.idEmpleado = idEmpleado;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.salario = salario;
+    }
+
+    public Empleado() {
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+    
+    //Setter
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    
+    //tostring
+
+    @Override
+    public String toString() {
+        return "Id. Empleado: " + idEmpleado + 
+                "\nNombre :" + nombre + 
+                "\nApellido: " + apellido + 
+                "\nSalario:" + salario;
+    }
+    
+}
