4d08f06ba77f6a4a988153e3417461e9cd1e3af8
Oscar <oscaralejandrosoto9@gmail.com>
2024-10-23 06:44:39 +0100

    Modelo terminado

--- src/java/Modelo/Nomina.java
+++ src/java/Modelo/Nomina.java
@@ -8,6 +8,66 @@ package Modelo;
  *
  * @author oscar
  */
-public class Nomina {
+public final class Nomina { //No se puede heredar esta clase por motivos de seguridad
+    private final Empleado empleado; // Los atributos nno van a cambiar de valor una vez esten inicializados
+    private final int diasTrabajados; 
+    private double salario; //este se calcula de acuerdo al salario base y los dias trabajados
+    private double descuentoSalud;
+    private double descuentoPension;
+    private double auxilioTransporte;
+    private double neto;
     
+    public Nomina(Empleado empleado, int diasTrabajados) {
+        this.empleado = empleado;
+        this.diasTrabajados = diasTrabajados;
+        calcular();
+    }
+    
+    public void calcular(){
+        // 1. Calculo del salario
+        salario = (empleado.getSalario() / 30) * diasTrabajados;
+        
+        //2 Calculo de los descuentos
+        descuentoSalud = salario * 0.04;
+        descuentoPension = salario * 0.04;
+        
+        //validar si el empleado tiene un salario base de dos smlv 
+        if(empleado.getSalario() >= 2*1300000){
+            auxilioTransporte = 0;
+        }else{
+            auxilioTransporte = 162000;
+        }
+        
+        //3. Calcular neto:
+        
+        neto = salario - descuentoSalud - descuentoPension + auxilioTransporte;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public int getDiasTrabajados() {
+        return diasTrabajados;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public double getDescuentoSalud() {
+        return descuentoSalud;
+    }
+
+    public double getDescuentoPension() {
+        return descuentoPension;
+    }
+
+    public double getAuxilioTransporte() {
+        return auxilioTransporte;
+    }
+
+    public double getNeto() {
+        return neto;
+    }
 }
